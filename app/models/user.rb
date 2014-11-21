e3c65b6bd0ae61ad45fe9a1fd79d096795da4d07
BenjaGross <Benja.J.Gross@gmail.com>
2014-11-21 15:50:19 -0500

    added pending and uncharged payments

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,43 +5,56 @@ class User < ActiveRecord::Base
   end
 
   def update_payment_totals(payment_info)
-    settled_charge_value, settled_charge_time, transaction_count, last_transaction_id, i = Array.new(5){0}
+    settled_charge_value, pending_charge_value, uncharged_value, settled_charge_time, pending_charge_time, uncharged_time, settled_count, pending_count, uncharged_count, last_transaction_id, i = Array.new(11){0}
 
+    last_transaction_id = payment_info["data"].first["id"]
     payment_info["data"].each do |transaction|
-      if transaction["status"] == "settled" && transaction["action"] == "charge" && transaction["actor"]["display_name"] != self.name
 
-        settled_charge_value = increment_settled_charge_value(transaction, settled_charge_value)
-        settled_charge_time = increment_settled_charge_time(transaction, settled_charge_time)
-    
-        if i == 0
-          last_transaction_id = transaction["id"]
-          i = 1
+      if transaction["action"] == "charge" && transaction["actor"]["display_name"] != self.name
+        if transaction["status"] == "settled" 
+          settled_charge_value = increment_value(transaction, settled_charge_value)
+          settled_charge_time = increment_time(transaction, settled_charge_time)
+          settled_count += 1
+        elsif transaction["status"] == "pending"
+          pending_charge_value = increment_value(transaction, pending_charge_value)
+          pending_charge_time = increment_time(transaction, pending_charge_time)
+          pending_count += 1
         end
-
-        transaction_count += 1
-      # elsif transaction["status"] == "pending"
-      #   find_pending
+      elsif transaction["action"] == "pay" && transaction["actor"]["display_name"] == self.name
+        uncharged_value = increment_value(transaction, uncharged_value)
+        uncharged_time = increment_time(transaction, uncharged_time)
+        uncharged_count += 1
        end
     end
 
     venmo_score = self.calculate_score(settled_charge_value, settled_charge_time)
-    self.update(settled_charge_value: settled_charge_value, settled_charge_time: settled_charge_time, last_transaction_id: last_transaction_id, venmo_score: venmo_score, transaction_count: transaction_count)
+
+    self.update(settled_charge_value: settled_charge_value, 
+                pending_charge_value: pending_charge_value, 
+                uncharged_value: uncharged_value,  
+                settled_charge_time: settled_charge_time, 
+                pending_charge_time: pending_charge_time,
+                uncharged_time: uncharged_time,  
+                settled_count: settled_count,
+                pending_count: pending_count,
+                uncharged_count: uncharged_count,
+                last_transaction_id: last_transaction_id, 
+                venmo_score: venmo_score)
   end
 
-  def increment_settled_charge_value(transaction, settled_charge_value)
-    settled_charge_value += transaction["amount"]
+  def increment_value(transaction, value)
+    value += transaction["amount"]
   end
 
-  def increment_settled_charge_time(transaction, settled_charge_time)
-    t1 = Time.parse(transaction["date_created"])
-    t2 = Time.parse(transaction["date_completed"])
-    time_to_payment = (t2 - t1)/60
-    settled_charge_time += time_to_payment
-  end
-
-  def find_pending
-    #return total value of pending payments
-    #return total time for those payments
+  def increment_time(transaction, time)
+    created_date = Time.parse(transaction["date_created"])
+    if !transaction["date_completed"]
+      completed_date = Time.now
+    else  
+      completed_date = Time.parse(transaction["date_completed"])
+    end
+    time_to_payment = (completed_date - created_date)/60
+    time += time_to_payment
   end
 
   def calculate_score(settled_charge_value, settled_charge_time)
--- db/migrate/20141120170619_create_users.rb
+++ db/migrate/20141120170619_create_users.rb
@@ -4,12 +4,14 @@ class CreateUsers < ActiveRecord::Migration
       t.string    :name
       t.integer   :venmo_uid
       t.integer   :venmo_score
-      t.integer   :transaction_count
       t.integer   :last_transaction_id
+      t.integer   :settled_count
       t.float     :settled_charge_value
       t.integer   :settled_charge_time
+      t.integer   :pending_count
       t.float     :pending_charge_value
       t.integer   :pending_charge_time
+      t.integer   :uncharged_count
       t.float     :uncharged_value
       t.integer   :uncharged_time
 
