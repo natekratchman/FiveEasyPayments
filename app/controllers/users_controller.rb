558296c76257922e2c4b10a25b983620f0c73f4f
BenjaGross <Benja.J.Gross@gmail.com>
2014-11-23 13:18:19 -0500

    change file path from kana to ben

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     # payment_info = JSON.parse(open("https://api.venmo.com/v1/payments?access_token=#{session[:token]}&limit=1000").read)
 
     # Development/Test
-    payment_info = JSON.parse(IO.read("/Users/kanaabe/Development/flatiron/FiveEasyPayments/app/controllers/seedhash.rb"))
+    payment_info = JSON.parse(IO.read("/Users/BenjaminGross/dev/FiveEasyPayments/app/controllers/seedhash.rb"))
     @current_user = User.create(name: "Kana")
 
     @current_user.parse_info(payment_info)
