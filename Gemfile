2b9e312bada470cd80c12e79c5024f9bb5d4a6a2
natekratchman <natecrashman@gmail.com>
2014-11-24 09:28:25 -0500

    modified database configuration to use pg on heroku

--- Gemfile
+++ Gemfile
@@ -3,8 +3,12 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.6'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# Use sqlite3 as the database for Active Record => using pg for production
+group :development do 
+  gem 'sqlite3'
+end
+# => using pg for production
+gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,19 +76,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
-  ## Adding this to tell Heroku to use postgres rather than sqlite3
-  # configure :production do
-    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-
-    ActiveRecord::Base.establish_connection(
-      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-      :host     => db.host,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
-      :encoding => 'utf8'
-    )
-  # end
-  ###
 end
