cf11ce837537e6961f31221b5e6ee11a10a8c1f5
Kanako Abe <kanaabe@Kanakos-MacBook-Pro.local>
2014-11-26 10:38:45 -0500

    fix circles to account for high ratio

--- app/assets/javascripts/total_paid_circles.js
+++ app/assets/javascripts/total_paid_circles.js
@@ -13,24 +13,24 @@ $(document).ready(function(){
 
     	if(settled_value>uncharged_value){
     		bigger = "settled";
-    		small_value = settled_value;
-    		big_value = uncharged_value;
-    	}else{
-    		bigger = "uncharged";
     		small_value = uncharged_value;
     		big_value = settled_value;
+    	}else{
+    		bigger = "uncharged";
+    		small_value = settled_value;
+    		big_value = uncharged_value;
     	}
-
+        //debugger;
     	var ratio = big_value/100;
 
     	if(bigger == "settled"){
     		var settled_pixels = 150;
-    		var uncharged_pixels = uncharged_value*ratio+25;
+    		var uncharged_pixels = uncharged_value/ratio+25;
     	}else{
-    		var settled_pixels = settled_value*ratios+25;
+    		var settled_pixels = settled_value/ratio+25;
     		var uncharged_pixels = 150;
     	}
-    	
+    	//debugger;
     	var str = '<svg width="320" height="320"><circle cx=160 cy=160 r="'+settled_pixels+'" stroke="#e0c0c0" stroke-width="4" fill="#d94e4e" /><text style="text-anchor: middle" x="160" y="165" font-family="sans-serif" font-size="20px" fill="white">$'+settled_value+'</text></svg>';
 			$( "#total_paid_circle_settled" ).append(str);
 
