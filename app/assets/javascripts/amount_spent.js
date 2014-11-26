70a0089510c4b912ae027af8743f5d83adb50c02
Kanako Abe <kanaabe@Kanakos-MacBook-Pro.local>
2014-11-25 19:07:36 -0500

    getting ready to merge

---
+++ app/assets/javascripts/amount_spent.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    var settled_value = parseInt($('#settled_value')[0].innerHTML);
+    var pending_value = parseInt($('#pending_value')[0].innerHTML);
+    var uncharged_value = parseInt($('#uncharged_value')[0].innerHTML);
+    //debugger;
+    $.getJSON("payments.json")
+    .done(function(payment){
+        var payments = payment;
+        
+        var data = {
+            "labels":["Settled", "Pending", "Uncharged"],
+            "datasets":[
+            {
+            data: [settled_value,pending_value,uncharged_value],
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            highlight: "#FF5A5E"
+          }]
+        };
+        var options = {
+            showTooltips: true,
+            tooltipTemplate: "<%if (label){%><%=label%>: $<%}%><%= value %>",
+        };
+
+        var ctx = document.getElementById("totalPaid").getContext("2d");
+        var myBarChart = new Chart(ctx).Bar(data, options);
+
+    });
+});
