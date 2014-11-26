16ba4a66eb8d0f14f04c291b881bd14fe47f09e6
Kanako Abe <kanaabe@Kanakos-MacBook-Pro.local>
2014-11-26 09:37:20 -0500

    fix payment hash model to generate correct json

--- app/models/payment_hash.rb
+++ app/models/payment_hash.rb
@@ -5,19 +5,22 @@ def self.add_to_payment_json(transaction,payments,type)
     if type=="settled"
       child = payments["children"][0]["children"].select{|h| h["name"]==transaction["actor"]["display_name"]}
       if child == []
-        child = payments["children"][0]["children"].push({"name"=>transaction["actor"]["display_name"],"children"=>[]})
+        payments["children"][0]["children"].push({"name"=>transaction["actor"]["display_name"],"children"=>[]})
+        child = payments["children"][0]["children"].select{|h| h["name"]==transaction["actor"]["display_name"]}
       end
       child[0]["children"].push({"name"=>transaction["note"],"amount"=>transaction["amount"],"size"=>1})
     elsif type == "pending"
       child = payments["children"][1]["children"].select{|h| h["name"]==transaction["actor"]["display_name"]}
       if child == []
-        child = payments["children"][1]["children"].push({"name"=>transaction["actor"]["display_name"],"children"=>[]})
+        payments["children"][1]["children"].push({"name"=>transaction["actor"]["display_name"],"children"=>[]})
+        child = payments["children"][1]["children"].select{|h| h["name"]==transaction["actor"]["display_name"]}
       end
       child[0]["children"].push({"name"=>transaction["note"],"amount"=>transaction["amount"],"size"=>1})
     elsif type == "uncharged"
       child = payments["children"][2]["children"].select{|h| h["name"]==transaction["target"]["user"]["display_name"]}
       if child == []
-        child = payments["children"][2]["children"].push({"name"=>transaction["target"]["user"]["display_name"],"children"=>[]})
+        payments["children"][2]["children"].push({"name"=>transaction["target"]["user"]["display_name"],"children"=>[]})
+        child = payments["children"][2]["children"].select{|h| h["name"]==transaction["target"]["user"]["display_name"]}
       end
       child[0]["children"].push({"name"=>transaction["note"],"amount"=>transaction["amount"],"size"=>1})
     end
