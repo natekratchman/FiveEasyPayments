00fc2d25e6fc52a1d0d9d64b206c4f59d24eada1
Kanako Abe <kanaabe@Kanakos-MacBook-Pro.local>
2014-11-26 09:39:52 -0500

    fixed sunburst circle, now click-able

--- app/assets/javascripts/transaction_count_sunburst.js
+++ app/assets/javascripts/transaction_count_sunburst.js
@@ -44,6 +44,7 @@ $(document).ready(function(){
         .on("click", click);
 
   function click(d) {
+    console.log("here");
       path.transition()
         .duration(750)
         .attrTween("d", arcTween(d));
@@ -94,27 +95,27 @@ $(document).ready(function(){
     return angle / Math.PI * 180;
   }
 
- text.attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; });
+ // text.attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; });
 
-  function click(d) {
-    text.transition().attr("opacity", 0);
+ //  function click(d) {
+ //    text.transition().attr("opacity", 0);
     
-    path.transition()
-      .duration(750)
-      .attrTween("d", arcTween(d))
-      .each("end", function(e, i) {
-        // check if the animated element's data e lies within the visible angle span given in d
-        if (e.x >= d.x && e.x < (d.x + d.dx)) {
-          // get a selection of the associated text element
-          var arcText = d3.select(this.parentNode).select("text");
-          // fade in the text element and recalculate positions
-          arcText.transition().duration(750)
-            .attr("opacity", 1)
-            .attr("transform", function() { return "rotate(" + computeTextRotation(e) + ")" })
-            .attr("x", function(d) { return y(d.y); });
-        }
-      });
-  }
+ //    path.transition()
+ //      .duration(750)
+ //      .attrTween("d", arcTween(d))
+ //      .each("end", function(e, i) {
+ //        // check if the animated element's data e lies within the visible angle span given in d
+ //        if (e.x >= d.x && e.x < (d.x + d.dx)) {
+ //          // get a selection of the associated text element
+ //          var arcText = d3.select(this.parentNode).select("text");
+ //          // fade in the text element and recalculate positions
+ //          arcText.transition().duration(750)
+ //            .attr("opacity", 1)
+ //            .attr("transform", function() { return "rotate(" + computeTextRotation(e) + ")" })
+ //            .attr("x", function(d) { return y(d.y); });
+ //        }
+ //      });
+ //  }
 
   });
 
