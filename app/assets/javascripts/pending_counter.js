b0aeda4042b1b475aff79c33199f811e23de7810
Kanako Abe <kanaabe@Kanakos-MBP.home>
2014-11-26 01:15:45 -0500

    only compute JS when on show page

--- app/assets/javascripts/pending_counter.js
+++ app/assets/javascripts/pending_counter.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-  $("#pending_time")
-  var minutesLabel = document.getElementById("minutes");
-  var secondsLabel = document.getElementById("seconds");
-  var totalSeconds = parseInt(minutesLabel.innerHTML)*60;
-  
-  setInterval(setTime, 1000);
-  function setTime()
-  {
-    ++totalSeconds;
-    secondsLabel.innerHTML = totalSeconds%60;
-    minutesLabel.innerHTML = parseInt(totalSeconds/60);
-  }
 
+  if (window.location.pathname == '/') {
+    var minutesLabel = document.getElementById("minutes");
+    var secondsLabel = document.getElementById("seconds");
+    var totalSeconds = parseInt(minutesLabel.innerHTML)*60;
+    
+    setInterval(setTime, 1000);
+    function setTime()
+    {
+      ++totalSeconds;
+      secondsLabel.innerHTML = totalSeconds%60;
+      minutesLabel.innerHTML = parseInt(totalSeconds/60);
+    }
+  }
 });
